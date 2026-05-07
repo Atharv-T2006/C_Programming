45d4e40dca58564c188bc0d88b9a2be1068f46e8
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-07 10:46:11 +0530

    Add files via upload

---
+++ additionfunctionX.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int addition(int no1, int no2)
+{
+    int ans=0;
+    ans=no1+no2;
+    return ans;
+}
+int main(){
+    
+    int ret=0,a=11,b=10;
+    ret=addition(a,b);
+    printf("addition is : %d",ret);
+
+    return 0;
+}
---
+++ return.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int display()
+{
+    return 11;  
+}
+int main(){
+    int ret=0;
+    ret=display();
+    printf("%d\n",ret);
+    return 0;
+}
