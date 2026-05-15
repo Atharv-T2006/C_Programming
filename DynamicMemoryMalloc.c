4a76a45d2cd97c76e35e1c2338b11710e8ba9367
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-15 20:00:27 +0530

    Add files via upload

---
+++ DynamicMemoryMalloc.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{   
+    int size = 0;
+
+    printf("Enter no. of elements\n");
+    scanf("%d",&size);
+
+    // dynamic memory allocation
+    float * marks = NULL;
+    marks = (float *)malloc(size*sizeof(float));
+
+
+     // iteration
+     for(int i=0;i<size;i++)
+     {
+        printf("Enter your marks of subject %d : \n",i);
+        scanf("%f",&marks[i]);
+    }
+
+    for(int j=0;j<size;j++){
+        printf("\nEntered marks of subject %d : ",j);
+        printf("%f",marks[j]);
+    }
+
+    free(marks);   // deallocation
+    
+
+    return 0;
+}
