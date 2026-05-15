5f0a8f443c3b118d5b0ed642f180412a04f34176
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-15 20:00:59 +0530

    Add files via upload

---
+++ StringCreate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char Arr[]={'H','e','l','l','o','\0'};
+    char Brr[6]={'H','e','l','l','o','\0'};
+    char Crr[]="Hello";
+
+    printf("%s",Arr);
+    printf("%s",Brr);
+    printf("%s",Crr);
+
+
+
+
+    return 0;
+}
