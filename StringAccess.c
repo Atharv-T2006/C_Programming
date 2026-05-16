32dce6fb02fcd21ec294c5dfd31c6e9d125ad120
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-16 10:58:30 +0530

    Add files via upload

---
+++ StringAccess.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char Arr[]={'H','e','l','l','o','\0'};
+    printf("%c\n",Arr[0]);
+    printf("%c\n",Arr[1]);
+    printf("%c\n",Arr[2]);
+    printf("%c\n",Arr[3]);
+    printf("%c\n",Arr[4]);
+    
+    return 0;
+}
