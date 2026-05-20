924f9f1dd53738d6d7552eb9a2faaf844a66db08
Atharv-T2006 <atharvtimane1@gmail.com>
2026-05-20 18:35:13 +0530

    Add files via upload

---
+++ Entrypoint.c
@@ -0,0 +1,13 @@
+#include "Marvellouss.h"
+
+int main()
+{
+    int Ans = 0;
+    Ans = Addition(11,10);
+    printf("Addition is : %d\n",Ans);
+
+    Ans = Substraction(11,10);
+     printf("Substraction is : %d\n",Ans);
+
+    return 0;
+}
---
+++ Marvellouss.h
@@ -0,0 +1,6 @@
+// Function definition
+#include<stdio.h>
+
+// function prototype / function declaration
+int Addition(int , int);
+int Substraction(int , int);
